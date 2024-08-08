9c45fb1bcdd134d15eab6cdfb52429f9e07c73ed
fxcksh <172133072+fxcksh@users.noreply.github.com>
2024-08-08 09:46:33 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+RESERVED_WORDS = [
+    'company', 'status', 'channel', 'administration', 'users', 'features', 'privacy', 'direct_messages', 'public',
+    'feedback', 'avatar', 'update', 'special', 'yourusername', 't.me', 'blog', 'security', 'service', 'webkiosk',
+    'theme', 'service', 'notification', 'notifications', 'announcements', 'account', 'phone', 'download', 'flash',
+    'official', 'profiles', 'groups', 'help', 'projects', 'settings', 'legal', 'email', 'accounts', 'prove', 'share',
+    'tests', 'private', 'topics', 'thread', 'video', 'staff', 'daddies', 'destroy', 'press', 'botfather', 'stickers',
+    'admin', 'telegram', 'storebot', 'toulouse', 'stickersbot', 'version', 'services', 'feedback', 'login', 'flash',
+    'theme', 'bakers', 'special', 'public', 'reports', 'empty', 'support', 'topic', 'terms', 'adult', 'contact',
+    'administrator', 'price', 'bedroom', 'landlord', 'secrecy', 'https', 'blogs', 'profile'
+]
